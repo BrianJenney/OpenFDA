202c5aa016e15c35d1ac78a837993e1074d44463
Jason Arnold <jason@thejasonfile.com>
2015-12-13 22:04:58 -0500

    centered content, pushing the button clears the side effects

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 function search(){
+	$("#effects").html('');
+	var key = "7PcXD9iQU905xvCopWU8Nqkiy8GoiEnwpCQkcl5O"
+	var searchTerm = document.getElementById("fdaSearch").value
 
-var key = "7PcXD9iQU905xvCopWU8Nqkiy8GoiEnwpCQkcl5O"
-var searchTerm = document.getElementById("fdaSearch").value
-
-$.ajax({
-   url: "https://api.fda.gov/drug/event.json?api_key="+ key + "&search=" + searchTerm+"&count=patient.reaction.reactionmeddrapt.exact",
-   dataType: "json",
-   success: function(data) {
-   	for(i=0;i<15;i++){
-    var results = (data.results[i].term)
-    $("body").append(results + " " + "<br>")
-
-    console.log(data.results[i].term)
-}
-   },
-   type: 'GET'
-
-});
-
+	$.ajax({
+	   	url: "https://api.fda.gov/drug/event.json?api_key="+ key + "&search=" + searchTerm+"&count=patient.reaction.reactionmeddrapt.exact",
+	   	dataType: "json",
+	   	success: function(data) {
+	   		for(i=0;i<15;i++){
+	    		var results = (data.results[i].term)
+	    		$("#effects").append(results + " " + "<br>")
+				console.log(data.results[i].term)
+			}
+	   	},
+	   type: 'GET'
+	});
 }
